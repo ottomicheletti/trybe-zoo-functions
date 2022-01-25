143b8d40591affcff3c2be648f3046c0c09b43d3
Otto Micheletti <michelettiotto@gmail.com>
2022-01-25 15:32:03 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,18 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (!ids) {
+    return [];
+  } else {
+    let result = [];
+    for (let index = 0; index < ids.length; index += 1) {
+      result.push(species.find((animal) => animal.id === ids[index]));
+    }
+    return result;
+  }
 }
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 module.exports = getSpeciesByIds;
