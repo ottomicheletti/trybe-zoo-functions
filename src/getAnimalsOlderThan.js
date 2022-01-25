ccb2382a581b5f4944ec9befb7bfa5d85198a474
Otto Micheletti <michelettiotto@gmail.com>
2022-01-25 16:07:28 -0300

    Requisito 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,7 +2,8 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  return species.filter((specie) => specie.name === animal)[0].residents.every((resident) => resident.age > age);
+  return species.filter((specie) => specie.name === animal)[0]
+    .residents.every((resident) => resident.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -4,15 +4,13 @@ const data = require('../data/zoo_data');
 function getSpeciesByIds(...ids) {
   if (!ids) {
     return [];
-  } else {
-    let result = [];
+  } {
+    const result = [];
     for (let index = 0; index < ids.length; index += 1) {
       result.push(species.find((animal) => animal.id === ids[index]));
     }
     return result;
   }
 }
-
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 module.exports = getSpeciesByIds;
