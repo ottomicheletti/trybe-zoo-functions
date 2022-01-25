6a27b8e75abeef042d1db6b7c2e978d784660e4d
Otto Micheletti <michelettiotto@gmail.com>
2022-01-25 17:58:32 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,26 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const superiors = employees.map((employee) => employee.managers);
+  const bosses = [];
+  superiors.forEach((superior) => superior.forEach((entry) => bosses.push(entry)));
+  return bosses.some((boss) => boss === id);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const related = [];
+  employees.forEach((employee) => employee.managers.forEach((entry) => {
+    if (entry === managerId) {
+      related.push(`${employee.firstName} ${employee.lastName}`);
+    }
+  }));
+  if (related.length > 0) {
+    return related;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
+
+// console.log(getRelatedEmployees('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 module.exports = { isManager, getRelatedEmployees };
