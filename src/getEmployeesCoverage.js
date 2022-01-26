09e2ca47b63f5ff9c79d5efc8884905736576d90
Otto Micheletti <michelettiotto@gmail.com>
2022-01-26 20:07:36 -0300

    Requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,80 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+const getSpeciesByIds = require('./getSpeciesByIds');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const chamaFuncionarioPeloId = (id) => employees.find((employee) => employee.id === id);
+const chamaFuncionarioPelosNomes = (nome) => employees
+  .find((employee) => employee.firstName === nome || employee.lastName === nome);
+
+function objName(obj) {
+  const dados = chamaFuncionarioPelosNomes(obj.name);
+
+  if (dados) {
+    const responsible = [];
+    const locals = [];
+    dados.responsibleFor.forEach((entry) => responsible
+      .push(getSpeciesByIds(entry)[0].name) && locals.push(getSpeciesByIds(entry)[0].location));
+
+    return {
+      id: `${dados.id}`,
+      fullName: `${dados.firstName} ${dados.lastName}`,
+      species: responsible,
+      locations: locals,
+    };
+  }
+  throw new Error('Informações inválidas');
+}
+
+function objId(obj) {
+  const dados = chamaFuncionarioPeloId(obj.id);
+
+  if (dados) {
+    const responsible = [];
+    const locals = [];
+    dados.responsibleFor.forEach((entry) => responsible
+      .push(getSpeciesByIds(entry)[0].name) && locals.push(getSpeciesByIds(entry)[0].location));
+
+    return {
+      id: `${dados.id}`,
+      fullName: `${dados.firstName} ${dados.lastName}`,
+      species: responsible,
+      locations: locals,
+    };
+  }
+  throw new Error('Informações inválidas');
+}
+
+function objVazio() {
+  const funcionarios = Object.values(employees);
+  const result = [];
+  funcionarios.forEach((funcionario, index) => {
+    const responsible = [];
+    const locals = [];
+    funcionario.responsibleFor.forEach((entry) => responsible
+      .push(getSpeciesByIds(entry)[0].name) && locals.push(getSpeciesByIds(entry)[0].location));
+
+    result.push(
+      {
+        id: `${funcionario.id}`,
+        fullName: `${funcionario.firstName} ${funcionario.lastName}`,
+        species: responsible,
+        locations: locals,
+      },
+    );
+  });
+  return result;
+}
+
+function getEmployeesCoverage(obj) {
+  if (!obj) {
+    return objVazio();
+  }
+  if (obj.name) {
+    return objName(obj);
+  }
+  if (obj.id) {
+    return objId(obj);
+  }
 }
 
 module.exports = getEmployeesCoverage;
