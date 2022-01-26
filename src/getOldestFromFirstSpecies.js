0401c38e83702daaa01fb54100074588e1be97d8
Otto Micheletti <michelettiotto@gmail.com>
2022-01-26 16:35:47 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const funcionario = employees.find((pessoa) => pessoa.id === id);
+  const primeiroAnimal = funcionario.responsibleFor[0];
+  const animais = species.find((bicho) => bicho.id === primeiroAnimal);
+  const animal = animais.residents.sort((a, b) => b.age - a.age)[0];
+
+  return [`${animal.name}`, `${animal.sex}`, animal.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
