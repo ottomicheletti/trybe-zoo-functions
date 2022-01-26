c89c62662ad029fa1d7a47677a53d78d42837a10
Otto Micheletti <michelettiotto@gmail.com>
2022-01-26 16:09:20 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -20,13 +20,4 @@ function calculateEntry(entrants) {
   }
 }
 
-// console.log(calculateEntry([
-//   { name: 'Lara Carvalho', age: 5 },
-//   { name: 'Frederico Moreira', age: 5 },
-//   { name: 'Pedro Henrique Carvalho', age: 5 },
-//   { name: 'Maria Costa', age: 18 },
-//   { name: 'Núbia Souza', age: 18 },
-//   { name: 'Carlos Nogueira', age: 50 },
-// ]));
-
 module.exports = { calculateEntry, countEntrants };
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,57 @@
+const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const officeHour = (dia) => {
+  const diaDaSemana = Object.entries(hours).find((entry) => entry[0] === dia);
+  return `Open from ${diaDaSemana[1].open}am until ${diaDaSemana[1].close}pm`;
+};
+
+const exhibition = (dia) => {
+  const exb = [];
+  species.map((animal) => animal.availability).forEach((entry, index) => {
+    if (entry.includes(dia)) {
+      exb.push(species[index].name);
+    }
+  });
+  return exb;
+};
+
+const animals = (animal) => species.find((bicho) => bicho.name === animal).availability;
+
+const fullSchedule = () => {
+  const semana = Object.keys(hours);
+  const horarios = {};
+
+  semana.forEach((dia) => {
+    const { open, close } = hours[dia];
+
+    switch (true) {
+    case dia === 'Monday':
+      horarios[dia] = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+      break;
+    default:
+      horarios[dia] = { officeHour: `Open from ${open}am until ${close}pm`,
+        exhibition: exhibition(dia) };
+    }
+  });
+  return horarios;
+};
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const regexDay = /[A-Z].*y/g;
+  const regexAnimal = /^[a-z].*[s]$/g;
+
+  switch (true) {
+  case scheduleTarget === 'Monday':
+    return { [scheduleTarget]: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } };
+  case regexDay.test(scheduleTarget):
+    return { [scheduleTarget]: { officeHour: officeHour(scheduleTarget),
+      exhibition: exhibition(scheduleTarget) } };
+  case regexAnimal.test(scheduleTarget):
+    return animals(scheduleTarget);
+  default:
+    return fullSchedule();
+  }
 }
 
 module.exports = getSchedule;
